9616847f3730b7ef98e6d0cae94376f6ce614420
jelebe <48688326+jelebe@users.noreply.github.com>
2025-01-31 12:50:57 +0100

    cambios login.js
    la configuración de firebase se hace en un archivo base separado

---
+++ frontend/public/js/firebaseConfig.js
@@ -0,0 +1,13 @@
+// frontend/public/js/firebaseConfig.js
+// archivo base configuracion firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDvCRxaQn27ElJUX9TP8bVZdICCTXpr0_w",
+    authDomain: "kissmap-63e5c.firebaseapp.com",
+    projectId: "kissmap-63e5c",
+    storageBucket: "kissmap-63e5c.firebasestorage.app",
+    messagingSenderId: "1019969866748",
+    appId: "1:1019969866748:web:1b3ae170b1b11bfcf10079",
+    measurementId: "G-B7Q73R8WX8"
+};
+
+export default firebaseConfig;
