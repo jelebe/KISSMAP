ea1b12cd5021fd2438892aa4dd2d83052d422e4f
jelebe <48688326+jelebe@users.noreply.github.com>
2025-02-04 17:12:10 +0100

    Update config.js

--- frontend/public/js/config.js
+++ frontend/public/js/config.js
@@ -2,9 +2,9 @@
 
 // frontend/public/js/config.js
 
-// Define la ruta base del proyecto
-const ROOT_URL = '/'; // Ruta raíz del proyecto
-const PUBLIC_URL = '/frontend/public'; // Ruta base de los recursos en frontend/public
+// Define las rutas base del proyecto
+const ROOT_URL = '/kissmap/'; // Subruta de GitHub Pages
+const PUBLIC_URL = '/kissmap/frontend/public'; // Ruta base de los recursos
 
 // Exporta la configuración
 export const CONFIG = {
